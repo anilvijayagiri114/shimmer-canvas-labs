343ca49293d4e67f866c72bd75d7c81afe576f26
simplenspicy <pavantejz990@gmail.com>
2026-03-10 01:54:59 +0530

    Update Index.tsx to include JourneyButton import and placement

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,34 +1,15 @@
-import { useState } from "react";
-import { AnimatePresence } from "framer-motion";
-import EnvelopeOpener from "@/components/EnvelopeOpener";
-import BirthdayHero from "@/components/BirthdayHero";
-import PraiseSection from "@/components/PraiseSection";
-import BirthdayCountdown from "@/components/BirthdayCountdown";
-import MemoriesSection from "@/components/MemoriesSection";
-import BirthdayLetter from "@/components/BirthdayLetter";
-import BirthdayFooter from "@/components/BirthdayFooter";
+import React from 'react';
+
+import BirthdayLetter from '@/components/BirthdayLetter';
+import JourneyButton from '@/components/JourneyButton';
 
 const Index = () => {
-  const [opened, setOpened] = useState(false);
-
-  return (
-    <div className="min-h-screen bg-background">
-      <AnimatePresence>
-        {!opened && <EnvelopeOpener onOpen={() => setOpened(true)} />}
-      </AnimatePresence>
-
-      {opened && (
-        <>
-          <BirthdayHero />
-          <PraiseSection />
-          <MemoriesSection />
-          <BirthdayCountdown />
-          <BirthdayLetter />
-          <BirthdayFooter />
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <BirthdayLetter />
+            <JourneyButton />
+        </div>
+    );
 };
 
 export default Index;
